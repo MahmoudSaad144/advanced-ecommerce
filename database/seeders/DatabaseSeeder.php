14f5e31713cc2180d589190d63069fc12a8211bf
shafey <twy281219965130@gmail.com>
2023-04-08 23:20:37 +0200

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             MainSeeder::class,
-            ProductSeeder::class
+            ProductSeeder::class,
             CategorySeeder::class
         ]);
     }
